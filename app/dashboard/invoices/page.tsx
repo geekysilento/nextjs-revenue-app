fd5680f61eb82644166985be6532f8a006a75f1a
geekysilento <adityakarna.ak@gmail.com>
2023-12-28 16:30:35 +0545

    Revert "Merge branch 'main' of https://github.com/geekysilento/nextjs-revenue-app"
    This reverts commit b2f12b824c644508042e587b53a09a9641f9784a, reversing
    changes made to 2e863ad1b4e6a059f3e7acde9b7e7c9d1e512cf5.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,4 @@
 import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/Search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';

c5ee0212082a4f5e76c6434ccd7731fab4d5318b
geekysilento <adityakarna.ak@gmail.com>
2023-12-28 15:34:28 +0545

    fix deployment issue

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,7 +6,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string){
+  function handleSearch(term: string) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
